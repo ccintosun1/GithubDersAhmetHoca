c2accf55d7e4be15aefc80b2c145f64bb893b87c
Cemil <ccintosun1@gmail.com>
2023-02-15 21:22:49 +0300

    new Project initial

---
+++ src/kisisel_kullanim/Cemil.java
@@ -0,0 +1,15 @@
+package kisisel_kullanim;
+
+public class Cemil {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("That's it");
+
+        System.out.println("Collaborator Cemil changes");
+
+
+    }
+}
